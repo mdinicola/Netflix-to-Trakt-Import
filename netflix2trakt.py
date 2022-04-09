f18b2ac21c68dc618f9c7e57a4a1157f027663ba
m4cola <mike@mdinicola.com>
2022-04-09 15:19:54 -0400

    fix error getting movie watched time

--- netflix2trakt.py
+++ netflix2trakt.py
@@ -193,10 +193,14 @@ if config.SYNC_TV_SHOWS:
 if config.SYNC_MOVIES:
     for movie in netflixHistory.movies:
         if movie.tmdbId is not None:
+            print("Adding movie to trakt:")
             for watchedTime in movie.watchedAt:
-                print("Adding movie to trakt:")
-                watchedTime = re.sub('[^0-9]', '.', watchedTime)
-                time = datetime.datetime.strptime(watchedTime + ' 20:15', config.CSV_DATETIME_FORMAT + ' %H:%M')
+                try:
+                    time = datetime.datetime.strptime(watchedTime + ' 20:15', config.CSV_DATETIME_FORMAT + ' %H:%M')
+                except ValueError:
+                    # try the date with a dot (also for backwards compatbility)
+                    watchedTime = re.sub('[^0-9]', '.', watchedTime)
+                    time = datetime.datetime.strptime(watchedTime + ' 20:15', config.CSV_DATETIME_FORMAT + ' %H:%M')
                 addInfo = {
                             "movies": [
                                 {
